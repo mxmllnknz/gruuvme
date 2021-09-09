042b59c7e79c803fd0098d0881d50fd901ad0646
mxmllnknz <mkunz@cs.hmc.edu>
2021-09-09 12:11:50 -0700

    successful Spotify API requests

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 import discord
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 client = discord.Client()
+
+async def fetchSpotifyAudio(url):
+    # use sp.track or sp.tracks() and a parameter(s) of spotify URL
+    track_dict = sp.track(url)
+    name = track_dict.get('name')
+    album_dict = track_dict.get('album')
+    artist = album_dict.get('artists')[0].get('name')
+    return name, artist
 
 @client.event
 async def on_ready():
@@ -15,13 +28,13 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
     
-    if message.content.startswith('$play'):
+    if message.content.startswith('$play') and 'spotify' in message.content:
         argArray = message.content.split()
-        arg = argArry[1]
+        arg = argArray[1]
 
         if (len(argArray) > 2):
             await message.channel.send('Expected one argument, got {num}', len(argArray))
         
-        #audio =  await fetchSpotifyAudio(arg)
+        audio =  await fetchSpotifyAudio(arg)
 
 client.run(os.getenv('TOKEN'))
